4310205a4bf45f5697990a4a2b43224894bd2d4e
Daniel Betoret <danielbetoret.dev@gmail.com>
2025-05-07 09:58:52 -0300

    basic wordpress config on windows

---
+++ deploy.ps1
@@ -0,0 +1,23 @@
+# get project name and port from user
+$docker_project_name = ''
+while (!$docker_project_name) {
+    $docker_project_name = Read-Host "Por favor, digite um nome de projeto: "
+}
+
+$wordpress_port = ''
+while (!$wordpress_port) {
+    $wordpress_port = Read-Host "Por favor, digite a porta em que o projeto estará disponível: "
+}
+
+$phpmyadmin_port = ''
+while (!$phpmyadmin_port) {
+    $phpmyadmin_port = Read-Host "Por favor, digite a porta para o phpmyadmin: "
+}
+
+[Environment]::SetEnvironmentVariable('DOCKER_PROJECT_NAME', $docker_project_name)
+[Environment]::SetEnvironmentVariable('WORDPRESS_PORT', $wordpress_port)
+[Environment]::SetEnvironmentVariable('PHPMYADMIN_PORT', $phpmyadmin_port)
+
+docker-compose \
+    --project-name $docker_project_name \
+    up
