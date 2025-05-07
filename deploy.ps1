dfe785ecfa8d02f9a33c10065c92fdd94e34581a
Daniel Betoret <danielbetoret.dev@gmail.com>
2025-05-07 12:28:38 -0300

    update with instructions for windows

--- deploy.ps1
+++ deploy.ps1
@@ -1,17 +1,17 @@
 # get project name and port from user
 $docker_project_name = ''
 while (!$docker_project_name) {
-    $docker_project_name = Read-Host "Por favor, digite um nome de projeto: "
+    $docker_project_name = Read-Host "Por favor, digite um nome de projeto"
 }
 
 $wordpress_port = ''
 while (!$wordpress_port) {
-    $wordpress_port = Read-Host "Por favor, digite a porta em que o projeto estará disponível: "
+    $wordpress_port = Read-Host "Por favor, digite a porta em que o projeto estará disponível"
 }
 
 $phpmyadmin_port = ''
 while (!$phpmyadmin_port) {
-    $phpmyadmin_port = Read-Host "Por favor, digite a porta para o phpmyadmin: "
+    $phpmyadmin_port = Read-Host "Por favor, digite a porta para o phpmyadmin"
 }
 
 [Environment]::SetEnvironmentVariable('DOCKER_PROJECT_NAME', $docker_project_name)
@@ -26,5 +26,6 @@ cat .env | ForEach-Object {
 }
 
 docker-compose `
+    --detach `
     --project-name $docker_project_name `
     up
